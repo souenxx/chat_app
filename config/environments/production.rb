97a45d33f1b69c0dc29293268d385dc30c5374f6
souen <souen_xx@yahoo.co.jp>
2020-01-18 01:41:47 +0000

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,9 +41,9 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.allowed_request_origins = [ /http:\/\/.*/ ]
+  #config.action_cable.url = 'wss://souen-chatapp.herokuapp.com/cable'
+  #config.action_cable.allowed_request_origins = [ 'http://souen-chatapp.herokuapp.com/', 'http://souen-chatapp.herokuapp.com/' ]
+  config.action_cable.allowed_request_origins = [ /http?:\/\/.*/ ]
 
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
