0d5b2febe9486195b391ee771bfd2fcfa5c4cf4a
souenxx <shun823-souen@docomo.ne.jp>
2020-05-06 19:48:55 +0900

    update toppage

--- app/assets/javascripts/toppages.js
+++ app/assets/javascripts/toppages.js
@@ -1,2 +1,9 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$('.card-body').hover(
+	function(){
+		$(this).find('card-text').addClass({'display':'block'})
+},
+	function(){
+		$(this).find('card-text').removeClass({'display':'block'})
+});
